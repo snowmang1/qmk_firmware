c4fdfbdbddf193f4e5af8b75e515e2effe555c23
Evan Drake <evanmdrake@icloud.com>
2022-08-28 16:28:53 -0700

    added colemak to keyboard

--- keyboards/planck/keymaps/snowmang1/keymap.c
+++ keyboards/planck/keymaps/snowmang1/keymap.c
@@ -17,6 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+int CAP = 0; // toggle for caps lock
 
 enum planck_layers {
   _QWERTY,
@@ -50,14 +51,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Brite| Ctrl | Alt  | GUI  |Lower | No   |Space |Raise | Shft | Down |  Up  |No    |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_NO,  KC_SPC,  RAISE,   KC_RSFT, KC_DOWN, KC_UP,   KC_CAPS
+    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_NO,  KC_SPC,  RAISE,   KC_RSFT, KC_DOWN, KC_UP,   KC_NO
+),
+
+/* Colemak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  '   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower | No   |Space |Raise | Shift| Down |  Up  | No   |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_COLEMAK] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RSFT, KC_DOWN, KC_UP,   KC_NO
 ),
 
 /* Lower
@@ -75,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, _______, _______,    KC_LEFT,    KC_VOLD, KC_VOLU, KC_RIGHT
 ),
 
 /* Raise
@@ -91,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_DEL,  KC_CAPS,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
@@ -128,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, QK_BOOT,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, KC_NO ,  PLOVER,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
@@ -153,17 +172,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case COLEMAK:
+    case KC_CAPS:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
+	#ifdef RGBLIGHT_ENABLE
+	      if(CAP == 0) {
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[0]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[1]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[2]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[3]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[4]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[5]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[6]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[7]);
+		      sethsv(HSV_AZURE, (LED_TYPE *)&led[8]);
+		      rgblight_set(); // Utility functions do not call rgblight_set() automatically, so they need to be called explicitly.
+			CAP = 1;
+	      }
+	      else {
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[0]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[1]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[2]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[3]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[4]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[5]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[6]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[7]);
+		      sethsv(HSV_PURPLE, (LED_TYPE *)&led[8]);
+		      rgblight_set(); // Utility functions do not call rgblight_set() automatically, so they need to be called explicitly.
+			CAP = 0;
+	      }
+        #endif
       }
-      return false;
-      break;
-    case DVORAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
-      }
-      return false;
+      return true; // let qmk send caps event
       break;
     case BACKLIT:
       if (record->event.pressed) {
